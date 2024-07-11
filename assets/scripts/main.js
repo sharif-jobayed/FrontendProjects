ed355eb9da181c5c27070976b34c4f5fcdef8904
sharif-jobayed <sharif.jobayed@gmail.com>
2024-07-11 13:59:27 +0600

    Add getNumber() & getOperator()

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,30 @@
-import { Calculator } from "./calculator.js";
 
-class Main {
-	constructor() {
+const previousOperandValue = document.querySelector(`[data-previousOperand]`).textContent.toString();
+const currentOperandValue = document.querySelector(`[data-currentOperand]`).textContent.toString();
+const numbers = document.querySelectorAll(`[data-number]`);
+const allClear = document.querySelector(`[data-allClear]`);
+const equals = document.querySelector(`[data-equals]`);
+const dlt = document.querySelector(`[data-delete]`);
+const operators = document.querySelectorAll(`[data-operation]`);
+let currentNumber;
+let currentOperator;
 
-		this.calculator = new Calculator();
-
-		this.previousOperand = document.querySelector(`data-previousOperand`);
-		this.currentOperand = document.querySelector(`data-currentOperand`);
-		this.numbers = document.querySelectorAll(`[data-number]`);
-		this.operators = document.querySelectorAll(`[data-operation]`);
-		this.allClear = document.querySelectorAll(`[data-allClear]`);
-		this.equals = document.querySelectorAll(`[data-equals]`);
-		this.deleteBtn = document.querySelectorAll(`[data-delete]`);
-	}
+const getNumber = () => {
+	numbers.forEach((n) => {
+		n.addEventListener(`click`, () => {
+			currentNumber = parseInt(n.textContent);
+			console.log(currentNumber);
+		});
+	});
 }
+getNumber();
 
-export { Main }
+const getOperator = () => {
+	operators.forEach((o) => {
+		o.addEventListener(`click`, () => {
+			currentOperator = o.textContent.toString();
+			console.log(currentOperator);
+		});
+	});
+}
+getOperator();
